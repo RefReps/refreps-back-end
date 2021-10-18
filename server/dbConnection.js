c1002558dd755d61061926978eb76e1f23235ef2
Hunter Durbin <hmdurbin@bsu.edu>
2021-10-18 15:49:55 -0400

    Change db connection to env var DB_CONNECT Add User to conn models

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -6,13 +6,16 @@ const dbHost = process.env.DB_HOST
 const dbPort = process.env.DB_PORT
 const dbName = process.env.DB_NAME
 
-const uri = `mongodb://${dbHost}:${dbPort}/${dbName}`
+const uri = process.env.DB_CONNECT
 
 // Create the connection to the mongodb
 const conn = mongoose.createConnection()
-conn.openUri(uri)
+conn.openUri(uri).catch((err) => {
+	console.log('could not connect to db')
+})
 
 // Create models from schemas for the conn object
 conn.model('Video', require(path.join(__dirname, '/./../schemas/video')))
+conn.model('User', require(path.join(__dirname, '/./../schemas/user')))
 
 module.exports = conn
