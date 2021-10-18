c933924aa43c6a8d7aa4be477932e5f6d792f536
Hunter Durbin <hmdurbin@bsu.edu>
2021-10-17 21:25:17 -0400

    add routes to courses router

--- routers/courses.js
+++ routers/courses.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/:id', (req, res) => {
+router.route('/new').post((req, res) => {
+	res.json({ msg: 'Making a new course' })
+})
+
+router.route('/:id').get((req, res) => {
+	if (isNaN(req.params.id)) {
+		res.json({ msg: 'Could not find course' })
+	}
 	res.json({ msg: `Welcome to course ${req.params.id}` })
+})
+
+router.route('/').get((req, res) => {
+	res.json({ msg: 'course index' })
 })
 
 module.exports = router
