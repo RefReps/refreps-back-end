55654e7287f9a90527c0bf879d9f7601c9866d1e
Hunter Durbin <hmdurbin@bsu.edu>
2021-10-25 15:58:17 -0400

    Remove config.json data variables

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose')
 const path = require('path')
 require('dotenv').config({ path: __dirname + '/./../.env' })
-const { db_connect } = require('../data/config.json')
 
 // Create the connection to the mongodb
 const conn = mongoose.createConnection()
-conn.openUri(db_connect).catch((err) => {
+conn.openUri(process.env.DB_CONNECT).catch((err) => {
 	console.log('could not connect to db')
 })
 
