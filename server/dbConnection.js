737c61870f2061ca093ae7572bcd75eb595ee1c0
Hunter Durbin <hmdurbin@bsu.edu>
2021-10-24 21:00:13 -0400

    Change db_connect import

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose')
 const path = require('path')
 require('dotenv').config({ path: __dirname + '/./../.env' })
-
-const dbHost = process.env.DB_HOST
-const dbPort = process.env.DB_PORT
-const dbName = process.env.DB_NAME
-
-const uri = process.env.DB_CONNECT
+const { db_connect } = require('../data/config.json')
 
 // Create the connection to the mongodb
 const conn = mongoose.createConnection()
-conn.openUri(uri).catch((err) => {
+conn.openUri(db_connect).catch((err) => {
 	console.log('could not connect to db')
 })
 
