57999f8b5708523bc59f0d98cbf1c1d3d15a1684
Hunter Durbin <hmdurbin@bsu.edu>
2021-10-27 21:36:43 -0400

    Add required to video schema

--- schemas/video.js
+++ schemas/video.js
@@ -4,27 +4,35 @@ const Schema = mongoose.Schema
 const videoInfo = {
 	fieldname: {
 		type: String,
+		required: true,
 	},
 	originalname: {
 		type: String,
+		required: true,
 	},
 	encoding: {
 		type: String,
+		required: true,
 	},
 	mimetype: {
 		type: String,
+		required: true,
 	},
 	destination: {
 		type: String,
+		required: true,
 	},
 	filename: {
 		type: String,
+		required: true,
 	},
 	path: {
 		type: String,
+		required: true,
 	},
 	size: {
 		type: Number,
+		required: true,
 	},
 }
 
