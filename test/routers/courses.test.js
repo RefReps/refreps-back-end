a7445c2fd1f05cb2ca7699ae43258ebd64b78399
Hunter Durbin <hmdurbin@bsu.edu>
2021-10-27 21:37:49 -0400

    Add dummy tests for routers

---
+++ test/routers/courses.test.js
@@ -0,0 +1,7 @@
+const router = require('../../routers/courses')
+
+describe('GET path~/', () => {
+	it('should recieve an array of documents of contents', (done) => {
+		done()
+	})
+})
---
+++ test/routers/modules.test.js
@@ -0,0 +1,31 @@
+const router = require('../../routers/modules')
+
+describe('GET path~/', () => {
+	it('should get an array of documents of modules', (done) => {
+		done()
+	})
+})
+
+describe('POST path~/', () => {
+	it('should post a new module', (done) => {
+		done()
+	})
+})
+
+describe('GET path~/:moduleId', () => {
+	it('should get a specific module document', (done) => {
+		done()
+	})
+})
+
+describe('PUT path~/:moduleId', () => {
+	it('should update a specific module document', (done) => {
+		done()
+	})
+})
+
+describe('DELETE path~/:moduleId', () => {
+	it('should delete a specific module document', (done) => {
+		done()
+	})
+})
