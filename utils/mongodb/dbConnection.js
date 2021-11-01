2c197788333a8479d2e63092f8c2b8240eed6b7c
Hunter Durbin <hmdurbin@bsu.edu>
2021-11-01 13:27:10 -0400

    Removed collection models not being used

--- utils/mongodb/dbConnection.js
+++ utils/mongodb/dbConnection.js
@@ -4,13 +4,8 @@ const mongoose = require('mongoose')
 const conn = mongoose.createConnection()
 
 // Create models from schemas for the conn object
-conn.model('Author', require('../../schemas/author').authorSchema)
 conn.model('Content', require('../../schemas/content').contentSchema)
 conn.model('Course', require('../../schemas/course').courseSchema)
-conn.model('Module', require('../../schemas/module').moduleSchema)
-conn.model('Section', require('../../schemas/section').sectionSchema)
-conn.model('Setting', require('../../schemas/setting').settingSchema)
-conn.model('Student', require('../../schemas/student').studentSchema)
 conn.model('User', require('../../schemas/user').userSchema)
 conn.model('Video', require('../../schemas/video').videoSchema)
 
