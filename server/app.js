3e140a2bc34cbff9468105d4d605bf07dbec1438
Hunter Durbin <hmdurbin@bsu.edu>
2021-11-01 13:26:25 -0400

    Remove removed routers

--- server/app.js
+++ server/app.js
@@ -13,23 +13,16 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // for parsing multipart/form-data
-// app.use(upload.none())
 app.use(express.static('public'))
 
 app.use(cors())
 
 // Import Routes
-const contentRouter = require('../routers/contents')
 const courseRouter = require('../routers/courses')
-const moduleRouter = require('../routers/modules')
-const sectionRouter = require('../routers/sections')
 const videoRouter = require('../routers/videos')
 
 // Route Middleware
-app.use('/api/content', contentRouter)
 app.use('/api/course', courseRouter)
-app.use('/api/module', moduleRouter)
-app.use('/api/section', sectionRouter)
 app.use('/api/video', videoRouter)
 
 module.exports = app
