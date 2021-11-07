7802cffb03e7215a5fc9c2fe6f33bca3124d9d48
Hunter Durbin <hmdurbin@bsu.edu>
2021-11-07 18:45:33 -0500

    Send a video by id

--- routers/videos.js
+++ routers/videos.js
@@ -3,6 +3,8 @@ const router = require('express').Router()
 // connection import
 const conn = require('../utils/mongodb/dbConnection')
 const upload = require('../utils/middleware/server-upload')
+const crudOperations = require('../utils/mongodb/crudOperations')
+const courseware = require('../utils/mongodb/course')
 require('dotenv').config({ path: '.env' })
 
 const db_uri = process.env.DB_CONNECT
@@ -19,22 +21,44 @@ router
 	// Post a new video in the database AND upload directory
 	.post(upload.single('video'), async (req, res) => {
 		try {
+			const { courseId, sectionId, moduleId } = req.body
+			if (!(courseId && moduleId && sectionId)) {
+				throw new Error('Must have courseId, sectionId, and moduleId')
+			}
 			if (!req.hasOwnProperty('file'))
 				throw new Error('req.file is needed to save video to db')
 			await conn.openUri(db_uri)
-			await videodb.saveNewVideo(req.file)
-			res.status(204).send()
+			const videoDoc = await videodb.saveNewVideo(req.file)
+			contentDoc = {
+				contentName: req.file.originalname.split('.')[0],
+				toContent: videoDoc._id,
+				onModel: 'Video',
+			}
+			console.log('yo')
+			await courseware.pushNewContent(courseId, sectionId, moduleId, contentDoc)
+			res.status(200).send('uo')
 		} catch (error) {
 			res.status(400).json()
+		} finally {
+			conn.close()
 		}
 	})
 
 router
 	.route('/:videoId')
 	// Get the video file to stream
-	.get((req, res) => {
+	.get(async (req, res) => {
 		// need api call to some api streaming
-		res.json({ msg: 'Get a specific video to stream' })
+		try {
+			await conn.openUri(db_uri)
+			const videoDoc = await videodb.getVideoById(req.params.videoId)
+			const videoObj = videoDoc.toObject()
+			res.status(200).json(videoObj)
+		} catch (error) {
+			res.status(400).send('oops')
+		} finally {
+			conn.close()
+		}
 	})
 	// Delete a specific video in the database AND upload directory
 	.delete((req, res) => {
