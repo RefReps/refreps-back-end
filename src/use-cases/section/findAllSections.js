169687a2a18b5a91322b133c93a87f5c77467e50
Hunter Durbin <hmdurbin@bsu.edu>
2021-11-30 09:11:58 -0500

    Add feature for finding sections

---
+++ src/use-cases/section/findAllSections.js
@@ -0,0 +1,40 @@
+module.exports = makeFindAllSections = ({ Section }) => {
+	// Finds all sections in the db
+	// Options: publishedOnly -> Find only published sections (default: true)
+	// Resolve -> {found: #, sections: {object}}
+	// Reject -> err.name
+	return async function findAllSections(
+		courseId,
+		{ publishedOnly = true } = {}
+	) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				if (!courseId) {
+					throw new ReferenceError('courseId is undefined')
+				}
+
+				let query = {}
+				query['courseId'] = courseId
+				if (publishedOnly) {
+					query['isPublished'] = true
+				}
+
+				const sectionQuery = Section.find(query)
+
+				sectionQuery.sort({ sectionOrder: 1, _id: 1 })
+				const sectionDocs = await sectionQuery.exec()
+
+				let sectionObjects = []
+				sectionDocs.forEach((doc) => {
+					sectionObjects.push(doc.toObject())
+				})
+				return resolve({
+					found: sectionObjects.length,
+					sections: sectionObjects,
+				})
+			} catch (err) {
+				return reject(err)
+			}
+		})
+	}
+}
---
+++ src/use-cases/section/findAllSections.test.js
@@ -0,0 +1,86 @@
+const Section = require('../../database/models/section.model')
+const { makeFakeSection } = require('../../../__test__/fixtures')
+const {
+	dbConnect,
+	dbDisconnect,
+} = require('../../utils/test-utils/dbHandler.utils')
+const makeAddSection = require('./addSection')
+const makeFindAllSections = require('./findAllSections')
+
+describe('findAllSections Test Suite', () => {
+	const addSection = makeAddSection({ Section })
+	const findAllSections = makeFindAllSections({ Section })
+
+	beforeAll(async () => {
+		await dbConnect()
+	})
+
+	beforeEach(async () => {
+		await Section.deleteMany({})
+	})
+
+	afterAll(async () => {
+		await dbDisconnect()
+	})
+
+	it('successfully finds all sections within a course', async () => {
+		const section1 = await addSection(
+			makeFakeSection({ name: 'section1', sectionOrder: 1 })
+		)
+		const section2 = await addSection(
+			makeFakeSection({ name: 'section2', sectionOrder: 2 })
+		)
+		const section3 = await addSection(
+			makeFakeSection({ name: 'section3', sectionOrder: 3 })
+		)
+		const section4 = await addSection(
+			makeFakeSection({ name: 'section4', sectionOrder: 4 })
+		)
+		const section5 = await addSection(
+			makeFakeSection({ name: 'section5', sectionOrder: 5 })
+		)
+		const diffSection = await addSection(
+			makeFakeSection({
+				name: 'not included',
+				courseId: '61a55df0eb19b75ef3139471',
+			})
+		)
+
+		const { courseId } = makeFakeSection()
+		const foundSections = await findAllSections(courseId)
+		expect(foundSections.found).toBe(5)
+		expect(foundSections.sections).toEqual([
+			section1.toObject(),
+			section2.toObject(),
+			section3.toObject(),
+			section4.toObject(),
+			section5.toObject(),
+		])
+	})
+
+	it('returns found of 0 if no sections are found', async () => {
+		const results = await findAllSections('61a55df0eb19b75ef3139471')
+		expect(results).toMatchObject({ found: 0, sections: [] })
+	})
+
+	it('rejects if no courseId is provided', async () => {
+		let errorMessage = 'nothing'
+		try {
+			await findAllSections()
+		} catch (error) {
+			errorMessage = error.message
+		}
+		expect(errorMessage).toBe('courseId is undefined')
+	})
+
+	// it('includes non published courses if specified', async () => {
+	// 	await addCourse(makeFakeCourse({ isPublished: false }))
+	// 	await addCourse(makeFakeCourse({ isPublished: true }))
+
+	// 	const resultsPublishedOnly = await findAllCourses({ publishedOnly: true })
+	// 	expect(resultsPublishedOnly.found).toBe(1)
+
+	// 	const resultsNonPublished = await findAllCourses({ publishedOnly: false })
+	// 	expect(resultsNonPublished.found).toBe(2)
+	// })
+})
