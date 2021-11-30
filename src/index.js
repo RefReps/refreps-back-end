a0cfda414e38b8d6fd095d75d1b30a494d7ebde6
Hunter Durbin <hmdurbin@bsu.edu>
2021-11-30 09:15:18 -0500

    Use course and section router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ require('dotenv').config({ path: '.env' })
 const { postCourse } = require('./controllers/index')
 const makeCallback = require('./express-callback/index')
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DB_URI)
+mongoose.connect(process.env.DB_CONNECT)
 
 const app = express()
 app.use(express.json())
 
-app.post('/api/course', makeCallback(postCourse))
+// app.post('/api/course', makeCallback(postCourse))
+
+// Require all routers
+const courseRouter = require('./routers/course')
+const sectionRouter = require('./routers/section')
+
+// Use Routers
+app.use('/api/course', courseRouter)
+app.use('/api/section', sectionRouter)
 
 app.listen(3000, () => {
 	console.log('Server is listening on port 3000')
