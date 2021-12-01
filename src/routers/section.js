b051909b168c417b64b050b1a135ba02b89e880f
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-01 02:25:16 -0500

    Add post section Add find section by id Add delete section Add update section

--- src/routers/section.js
+++ src/routers/section.js
@@ -6,6 +6,7 @@ const useCases = require('../use-cases/index')
 
 router
 	.route('/')
+	// findAllSections
 	.get(async (req, res) => {
 		try {
 			const { courseId } = req.query
@@ -18,10 +19,12 @@ router
 			res.status(400).send(error.message)
 		}
 	})
+	// addSection
 	.post(multer.none(), async (req, res) => {
 		try {
 			const result = await useCases.section.addSection(req.body)
-			res.send(result)
+			await useCases.section.collapseSection(req.body.courseId)
+			res.status(204).send()
 		} catch (error) {
 			res.status(400).send(error)
 		}
@@ -29,25 +32,51 @@ router
 
 router
 	.route('/:sectionId')
+	// findSectionById
 	.get(async (req, res) => {
 		try {
-		} catch (error) {
-			res.status(400).send(error)
-		}
-	})
-	.delete(async (req, res) => {
-		try {
-		} catch (error) {
-			res.status(400).send(error)
-		}
-	})
-	.put(multer.none(), async (req, res) => {
-		try {
 			const { sectionId } = req.params
-			const result = await useCases.section.updateSection(sectionId, req.body)
+			const result = await useCases.section.findSectionById(sectionId)
 			res.send(result)
 		} catch (error) {
 			res.status(400).send(error)
+		}
+	})
+	// deleteSection
+	.delete(async (req, res) => {
+		const { sectionId } = req.params
+		try {
+			const section = await useCases.section.findSectionById(sectionId)
+			await useCases.section.deleteSection(sectionId)
+			await useCases.section.collapseSection(section.courseId)
+			res.status(204).send()
+		} catch (error) {
+			res.status(400).send(error)
+		}
+	})
+	// updateSection
+	.put(multer.none(), async (req, res) => {
+		// Update order if req.body.sectionOrder
+		const { sectionId } = req.params
+		const { name, courseId, isPublished, sectionOrder, dropDate } = req.body
+		try {
+			if (sectionOrder) {
+				await useCases.section.moveSectionOrder(sectionId, sectionOrder)
+				const section = await useCases.section.findSectionById(sectionId)
+				await useCases.section.collapseSection(section.courseId)
+			}
+
+			const sectionInfo = {}
+			sectionInfo['name'] = name ? name : undefined
+			sectionInfo['isPublished'] = isPublished ? isPublished : undefined
+			sectionInfo['dropDate'] = dropDate ? dropDate : undefined
+			const result = await useCases.section.updateSection(
+				sectionId,
+				sectionInfo
+			)
+			res.send(result)
+		} catch (error) {
+			res.status(400).send(error.toString())
 		}
 	})
 
