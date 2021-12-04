f19ec88c74b34d235e7491d94e33e516899e4b2e
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-04 17:21:59 -0500

    Expose content and video use cases

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -1,9 +1,13 @@
+const content = require('./content/index')
 const course = require('./course/index')
 const module_ = require('./module/index')
 const section = require('./section/index')
+const video = require('./video/index')
 
 module.exports = {
+	content,
 	course,
 	module_,
 	section,
+	video,
 }
