8edf97680c1d9d72f2308eb5afc20d3f579f06c8
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-06 12:00:40 -0500

    Move old utils to new utils directory

---
+++ src/utils/checkEnvFile.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ path: '.env' })
+
+const criticalEnvVariables = [
+	'HOST',
+	'PORT',
+	'DB_CONNECT',
+	// 'TOKEN_SECRET',
+	'LOCAL_UPLOAD_PATH',
+]
+
+module.exports.checkAllCriticalEnvVariablesExists = () => {
+	let envObj = {}
+	criticalEnvVariables.forEach((ele) => {
+		envObj[ele] = process.env[ele]
+	})
+	let errorList = []
+	Object.entries(envObj).forEach(([key, val]) => {
+		if (val === '' || val === undefined) {
+			errorList.push(`Could not find ${key} in env file`)
+		}
+	})
+	if (errorList.length > 0) {
+		console.error(errorList.toString())
+		return false
+	}
+	return true
+}
