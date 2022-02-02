817b9a1a0c7ea205fe734bc3bcf027c804c3b7ec
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-02 01:08:51 -0500

    Expose copyCourse

--- src/use-cases/course/index.js
+++ src/use-cases/course/index.js
@@ -1,6 +1,7 @@
 const Course = require('../../database/models/course.model')
 
 const makeAddCourse = require('./addCourse')
+const makeCopyCourse = require('./copyCourse')
 const makeDeleteCourse = require('./deleteCourse')
 const makeFindAllCourses = require('./findAllCourses')
 const makeFindCourseById = require('./findCourseById')
@@ -8,6 +9,7 @@ const makeUpdateCourse = require('./updateCourse')
 
 module.exports = {
 	addCourse: makeAddCourse({ Course }),
+	copyCourse: makeCopyCourse({ Course }),
 	deleteCourse: makeDeleteCourse({ Course }),
 	findAllCourses: makeFindAllCourses({ Course }),
 	findCourseById: makeFindCourseById({ Course }),
