85fe8dc9ef1945fe1100e6b9c14d4deac258feb3
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-03 15:22:19 -0500

    Expose deleteQuestion

--- src/use-cases/quiz/index.js
+++ src/use-cases/quiz/index.js
@@ -3,14 +3,17 @@ const QuizJson = require('../../utils/quiz/quizJson')
 
 const makeAddQuestion = require('./addQuestion')
 const makeAddQuiz = require('./addQuiz')
+const makeDeleteQuestion = require('./deleteQuestion')
 const makeFindQuizById = require('./findQuizById')
 
 const addQuestion = makeAddQuestion({ Quiz, QuizJson })
 const addQuiz = makeAddQuiz({ Quiz, QuizJson })
+const deleteQuestion = makeDeleteQuestion({ Quiz, QuizJson })
 const findQuizById = makeFindQuizById({ Quiz, QuizJson })
 
 module.exports = {
 	addQuestion,
 	addQuiz,
+	deleteQuestion,
 	findQuizById,
 }
