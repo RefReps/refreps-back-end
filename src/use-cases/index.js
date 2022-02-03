ac4393ed5c4af5cd0343b04ef53ab2031d13f551
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-02 20:28:12 -0500

    Expose quiz

--- src/use-cases/index.js
+++ src/use-cases/index.js
@@ -1,6 +1,7 @@
 const content = require('./content/index')
 const course = require('./course/index')
 const module_ = require('./module/index')
+const quiz = require('./quiz/index')
 const section = require('./section/index')
 const video = require('./video/index')
 
@@ -8,6 +9,7 @@ module.exports = {
 	content,
 	course,
 	module_,
+	quiz,
 	section,
 	video,
 }
