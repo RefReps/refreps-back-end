ba7cc8f63b5db4e18e2f10a9a2b890873dad7416
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-04 16:46:46 -0500

    Add copy cases for types of content stored

--- src/routers/course.js
+++ src/routers/course.js
@@ -56,11 +56,13 @@ router
 router.route('/:courseId/copy').post(multer.none(), async (req, res) => {
 	try {
 		const { courseId } = req.params
+		const overrides = req.body
 
-		const course = await useCases.course.copyCourse(courseId)
+		const course = await useCases.course.copyCourse(courseId, overrides)
 
 		const { sections } = await useCases.section.findAllSections(courseId)
 		sections.forEach(async (section) => {
+			// Copy all sections and bind them to the new course
 			let sectionCopy = await useCases.section.copySection(
 				section._id,
 				course._id
@@ -68,15 +70,35 @@ router.route('/:courseId/copy').post(multer.none(), async (req, res) => {
 
 			let { modules } = await useCases.module_.findAllModules(section._id)
 			modules.forEach(async (module_) => {
+				// Copy all modules and bind them to the new section, resp.
 				let moduleCopy = await useCases.module_.copyModule(
 					module_._id,
 					sectionCopy._id
 				)
 				let { contents } = await useCases.content.findAllContents(module_._id)
 				contents.forEach(async (content) => {
-					await useCases.content.copyContent(content._id, moduleCopy._id)
+					// Copy the document that the Content points to (if needed)
+					let bindDocumentId
+					switch (content.onModel) {
+						case 'Video':
+							bindDocumentId = content.toDocument
+							break
+						case 'Quiz':
+							const quiz = await useCases.quiz.copyQuiz(content.toDocument)
+							bindDocumentId = quiz._id
+						default:
+							bindDocumentId = content.toDocument
+					}
 
-					// If video -> dont do anything else
+					// Copy the Content, bind the Content to its resp Module, and
+					// bind the document
+					await useCases.content.copyContent(
+						content._id,
+						moduleCopy._id,
+						bindDocumentId
+					)
+
+					// TODO: If video -> dont do anything else
 					// If quiz -> copy the quiz (change toDocument in content) so that it can be edited
 				})
 			})
