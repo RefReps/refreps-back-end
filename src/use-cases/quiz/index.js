5e4ccf31f4209be141913720f88bd4c2f4936d3c
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-04 16:45:06 -0500

    Expose copyQuiz

--- src/use-cases/quiz/index.js
+++ src/use-cases/quiz/index.js
@@ -3,17 +3,20 @@ const QuizJson = require('../../utils/quiz/quizJson')
 
 const makeAddQuestion = require('./addQuestion')
 const makeAddQuiz = require('./addQuiz')
+const makeCopyQuiz = require('./copyQuiz')
 const makeDeleteQuestion = require('./deleteQuestion')
 const makeFindQuizById = require('./findQuizById')
 
 const addQuestion = makeAddQuestion({ Quiz, QuizJson })
 const addQuiz = makeAddQuiz({ Quiz, QuizJson })
+const copyQuiz = makeCopyQuiz({ Quiz, QuizJson })
 const deleteQuestion = makeDeleteQuestion({ Quiz, QuizJson })
 const findQuizById = makeFindQuizById({ Quiz, QuizJson })
 
 module.exports = {
 	addQuestion,
 	addQuiz,
+	copyQuiz,
 	deleteQuestion,
 	findQuizById,
 }
