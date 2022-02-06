c847a492bf20ce2b5945117b2d50c04ae1fb2e1c
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-05 23:57:23 -0500

    Init

---
+++ src/use-cases/user/addUser.js
@@ -0,0 +1,21 @@
+// Encryption will most likely be `bcrypt`
+module.exports = makeAddUser = ({ User, Encryption }) => {
+	// Save a new user in the db
+	// Resolve -> user object
+	// Reject -> error name
+	return async function addUser(userInfo = { email, password, role: 'user' }) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const userData = Object.assign({}, { ...userInfo })
+				userData.password = await Encryption.hash(userInfo.password, 10)
+
+				const user = new User(userData)
+
+				const saved = await user.save()
+				return resolve(saved.toObject())
+			} catch (err) {
+				return reject(err)
+			}
+		})
+	}
+}
---
+++ src/use-cases/user/appendRefreshToken.js
@@ -0,0 +1,30 @@
+module.exports = makeAppendRefreshToken = ({ User }) => {
+	// Updates an existing user
+	// Resolve -> updated user document
+	// Rejects -> error
+
+	// NOTE: this function does not ensure any specific parts of the user are
+	// not updated. That is handled inside of the controller that uses this function.
+	return async function appendRefreshToken(id, token) {
+		return new Promise(async (resolve, reject) => {
+			const options = { returnDocument: 'after' }
+			try {
+				if (!id) {
+					throw new ReferenceError('`id` is required to update')
+				}
+
+				const updated = await User.findByIdAndUpdate(
+					id,
+					{ $push: { refreshTokens: token } },
+					options
+				).exec()
+				if (updated == null) {
+					throw ReferenceError('User not found')
+				}
+				return resolve(updated.toObject())
+			} catch (error) {
+				return reject(error)
+			}
+		})
+	}
+}
