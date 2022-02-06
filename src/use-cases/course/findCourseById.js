2ed709a45bec752eb1bf898d5cef8561178531e4
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-06 16:38:35 -0500

    Add option for userEmail to know if author

--- src/use-cases/course/findCourseById.js
+++ src/use-cases/course/findCourseById.js
@@ -1,10 +1,10 @@
-module.exports = makeFindCourseById = ({ Course }) => {
+module.exports = makeFindCourseById = ({ Course, User }) => {
 	// Finds a course by an ObjectId
 	// Resolve -> {found: #, course: {object}}
 	// Reject -> error
 	return async function findCourseById(
 		id,
-		{ publishedOnly = true, includeDeleted = false } = {}
+		{ publishedOnly = true, includeDeleted = false, userEmail = '' } = {}
 	) {
 		return new Promise(async (resolve, reject) => {
 			try {
@@ -21,6 +21,11 @@ module.exports = makeFindCourseById = ({ Course }) => {
 					return resolve({ found: 0, course: {} })
 				}
 				const found = courseDoc.toObject()
+
+				if (userEmail) {
+					const user = await User.findOne({ email: userEmail })
+					found.isAuthor = user.authorCourses.includes(id)
+				}
 
 				return resolve({ found: 1, course: found })
 			} catch (error) {
