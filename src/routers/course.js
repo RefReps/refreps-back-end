0b97aa76b3fc48b1f234c4a5af310e724dc23924
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-06 00:02:44 -0500

    Add isAuthenticated middleware to router Add routes for adding/removing students/authors to courses

--- src/routers/course.js
+++ src/routers/course.js
@@ -3,20 +3,32 @@ require('dotenv').config({ path: '.env' })
 const multer = require('multer')()
 
 const useCases = require('../use-cases/index')
+const { User, Course } = require('../use-cases/index')
+const { isAuthenticated } = require('../utils/middleware/auth')
+
+router.use(isAuthenticated)
 
 router
 	.route('/')
 	.get(async (req, res) => {
 		try {
-			const result = await useCases.course.findAllCourses()
+			const user = await User.findUserByEmail(req.email)
+			let result
+			if (user.role === 'admin') {
+				result = await Course.findAllCourses()
+			} else {
+				result = await Course.findAllCoursesForUser(user._id)
+			}
 			res.send(result.courses)
 		} catch (error) {
-			res.status(400).send(error)
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
 		}
 	})
 	.post(multer.none(), async (req, res) => {
 		try {
-			const course = await useCases.course.addCourse(req.body)
+			const course = await Course.addCourse(req.body)
 			res.send(course)
 		} catch (error) {
 			res.status(400).send(error)
@@ -28,7 +40,7 @@ router
 	.get(async (req, res) => {
 		try {
 			const { courseId } = req.params
-			const result = await useCases.course.findCourseById(courseId)
+			const result = await Course.findCourseById(courseId)
 			res.send(result.course)
 		} catch (error) {
 			res.status(400).send(error)
@@ -37,7 +49,7 @@ router
 	.delete(async (req, res) => {
 		try {
 			const { courseId } = req.params
-			const result = await useCases.course.deleteCourse(courseId)
+			const result = await Course.deleteCourse(courseId)
 			res.send(result)
 		} catch (error) {
 			res.status(400).send(error)
@@ -46,10 +58,70 @@ router
 	.put(multer.none(), async (req, res) => {
 		try {
 			const { courseId } = req.params
-			const result = await useCases.course.updateCourse(courseId, req.body)
+			const result = await Course.updateCourse(courseId, req.body)
 			res.send(result.course)
 		} catch (error) {
 			res.status(400).send(error)
+		}
+	})
+
+// Admin only route
+router
+	.route('/:courseId/author/:email')
+	// Adds a user as an author to the course
+	.post(async (req, res) => {
+		try {
+			const { email, courseId } = req.params
+			const user = await User.findUserByEmail(email)
+			await User.addAuthorInCourse(user._id, courseId)
+			res.status(204).send()
+		} catch (error) {
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
+		}
+	})
+	// Removes a user as an author to the course
+	.delete(async (req, res) => {
+		try {
+			const { email, courseId } = req.params
+			const user = await User.findUserByEmail(email)
+			await User.removeAuthorInCourse(user._id, courseId)
+			res.status(204).send()
+		} catch (error) {
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
+		}
+	})
+
+// Admin route, and for authors that are in that course
+router
+	.route('/:courseId/student/:email')
+	// Adds a user as a student to the course
+	.post(async (req, res) => {
+		try {
+			const { email, courseId } = req.params
+			const user = await User.findUserByEmail(email)
+			await User.addStudentInCourse(user._id, courseId)
+			res.status(204).send()
+		} catch (error) {
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
+		}
+	})
+	// Removes a user as a student to the course
+	.delete(async (req, res) => {
+		try {
+			const { email, courseId } = req.params
+			const user = await User.findUserByEmail(email)
+			await User.removeStudentInCourse(user._id, courseId)
+			res.status(204).send()
+		} catch (error) {
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
 		}
 	})
 
@@ -58,24 +130,24 @@ router.route('/:courseId/copy').post(multer.none(), async (req, res) => {
 		const { courseId } = req.params
 		const overrides = req.body
 
-		const course = await useCases.course.copyCourse(courseId, overrides)
+		const course = await Course.copyCourse(courseId, overrides)
 
-		const { sections } = await useCases.section.findAllSections(courseId)
+		const { sections } = await useCases.Section.findAllSections(courseId)
 		sections.forEach(async (section) => {
 			// Copy all sections and bind them to the new course
-			let sectionCopy = await useCases.section.copySection(
+			let sectionCopy = await useCases.Section.copySection(
 				section._id,
 				course._id
 			)
 
-			let { modules } = await useCases.module_.findAllModules(section._id)
+			let { modules } = await useCases.Module_.findAllModules(section._id)
 			modules.forEach(async (module_) => {
 				// Copy all modules and bind them to the new section, resp.
-				let moduleCopy = await useCases.module_.copyModule(
+				let moduleCopy = await useCases.Module_.copyModule(
 					module_._id,
 					sectionCopy._id
 				)
-				let { contents } = await useCases.content.findAllContents(module_._id)
+				let { contents } = await useCases.Content.findAllContents(module_._id)
 				contents.forEach(async (content) => {
 					// Copy the document that the Content points to (if needed)
 					let bindDocumentId
@@ -84,7 +156,7 @@ router.route('/:courseId/copy').post(multer.none(), async (req, res) => {
 							bindDocumentId = content.toDocument
 							break
 						case 'Quiz':
-							const quiz = await useCases.quiz.copyQuiz(content.toDocument)
+							const quiz = await useCases.Quiz.copyQuiz(content.toDocument)
 							bindDocumentId = quiz._id
 						default:
 							bindDocumentId = content.toDocument
@@ -92,7 +164,7 @@ router.route('/:courseId/copy').post(multer.none(), async (req, res) => {
 
 					// Copy the Content, bind the Content to its resp Module, and
 					// bind the document
-					await useCases.content.copyContent(
+					await useCases.Content.copyContent(
 						content._id,
 						moduleCopy._id,
 						bindDocumentId
