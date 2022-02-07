2946add16338337651e8df3eb862f7b1498a064e
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-07 00:55:17 -0500

    Add routes for grade, save ,start

--- src/routers/quiz.js
+++ src/routers/quiz.js
@@ -3,7 +3,11 @@ require('dotenv').config({ path: '.env' })
 const multer = require('multer')()
 
 const useCases = require('../use-cases/index')
+const { User, Quiz } = require('../use-cases/index')
 const quizJson = require('../utils/quiz/quizJson')
+
+// Middleware Imports
+const { isAuthenticated } = require('../utils/middleware/auth')
 
 router
 	.route('/')
@@ -50,22 +54,17 @@ router
 		}
 	})
 	// Add new question
-	.post(multer.none(), async (req, res) => {
+	// req.body must be JSON
+	.post(async (req, res) => {
 		try {
 			const { quizId } = req.params
-			const { questionNumber, type, question, answers, answer, A, B, C, D } =
+			const { type, questionNumber, question, responses, answers, answer } =
 				req.body
-			responses = {
-				A,
-				B,
-				C,
-				D,
-			}
 
 			const quiz = await useCases.Quiz.findQuizById(quizId)
 			const result = await useCases.Quiz.addQuestion(
 				quiz._id,
-				questionNumber || 100,
+				questionNumber || 200,
 				{
 					type,
 					question,
@@ -83,7 +82,7 @@ router
 		}
 	})
 
-router.route('/:quizId/:questionNumber').delete(async (req, res) => {
+router.route('/:quizId/number/:questionNumber').delete(async (req, res) => {
 	try {
 		const { quizId, questionNumber } = req.params
 		await useCases.Quiz.deleteQuestion(quizId, questionNumber)
@@ -95,12 +94,74 @@ router.route('/:quizId/:questionNumber').delete(async (req, res) => {
 	}
 })
 
-router.route('/:courseId/copy').post(multer.none(), async (req, res) => {
-	try {
-		res.status(201).send()
-	} catch (error) {
-		res.status(400).send(error)
-	}
-})
+router
+	.route('/:quizId/start')
+	// Start a quiz
+	.get(isAuthenticated, async (req, res) => {
+		try {
+			const { email } = req
+			const { quizId } = req.params
+			if (!email) {
+				throw ReferenceError('req.email needs to be provided')
+			}
+			const user = await User.findUserByEmail(email)
+			const quizData = await Quiz.startQuiz(quizId, user._id)
+			res.status(200).json(quizData)
+		} catch (error) {
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
+		}
+	})
+
+router
+	.route('/:quizId/save')
+	// Save incoming userAnswers to the quiz
+	// req.body = {"1": "ABC", "2": "A", ...} must be provided (json)
+	.put(isAuthenticated, async (req, res) => {
+		try {
+			const { email } = req
+			const { quizId } = req.params
+			const userAnswers = req.body
+			if (!email) {
+				throw ReferenceError('req.email needs to be provided')
+			}
+			if (!userAnswers) {
+				throw ReferenceError('req.body needs to be provided')
+			}
+			const user = await User.findUserByEmail(email)
+			const quizData = await Quiz.saveAnswersInSubmission(
+				quizId,
+				user._id,
+				userAnswers
+			)
+			res.status(200).json(quizData)
+		} catch (error) {
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
+		}
+	})
+
+router
+	.route('/:quizId/grade')
+	// Grade and finish a quiz that was in progress
+	.post(isAuthenticated, async (req, res) => {
+		try {
+			const { email } = req
+			const { quizId } = req.params
+			if (!email) {
+				throw ReferenceError('req.email needs to be provided')
+			}
+			const user = await User.findUserByEmail(email)
+			const graded = await Quiz.gradeSubmission(quizId, user._id)
+
+			res.status(200).json(graded)
+		} catch (error) {
+			res
+				.status(400)
+				.json({ success: false, error: error.name, reason: error.message })
+		}
+	})
 
 module.exports = router
