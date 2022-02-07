8043c50b47bb7cbb2d08a415027f1d0f863eb8bf
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-07 00:53:21 -0500

    Expose new quiz cases

--- src/use-cases/quiz/index.js
+++ src/use-cases/quiz/index.js
@@ -1,4 +1,5 @@
 const Quiz = require('../../database/models/quiz.model')
+const QuizSubmission = require('../../database/models/quizSubmission.model')
 const QuizJson = require('../../utils/quiz/quizJson')
 
 const makeAddQuestion = require('./addQuestion')
@@ -6,12 +7,18 @@ const makeAddQuiz = require('./addQuiz')
 const makeCopyQuiz = require('./copyQuiz')
 const makeDeleteQuestion = require('./deleteQuestion')
 const makeFindQuizById = require('./findQuizById')
+const makeGradeSubmission = require('./gradeSubmission')
+const makeSaveAnswersInSubmission = require('./saveAnswersInSubmission')
+const makeStartQuiz = require('./startQuiz')
 
 const addQuestion = makeAddQuestion({ Quiz, QuizJson })
 const addQuiz = makeAddQuiz({ Quiz, QuizJson })
 const copyQuiz = makeCopyQuiz({ Quiz, QuizJson })
 const deleteQuestion = makeDeleteQuestion({ Quiz, QuizJson })
 const findQuizById = makeFindQuizById({ Quiz, QuizJson })
+const gradeSubmission = makeGradeSubmission({ Quiz, QuizJson, QuizSubmission })
+const saveAnswersInSubmission = makeSaveAnswersInSubmission({ QuizSubmission })
+const startQuiz = makeStartQuiz({ Quiz, QuizSubmission, QuizJson })
 
 module.exports = {
 	addQuestion,
@@ -19,4 +26,7 @@ module.exports = {
 	copyQuiz,
 	deleteQuestion,
 	findQuizById,
+	gradeSubmission,
+	saveAnswersInSubmission,
+	startQuiz,
 }
