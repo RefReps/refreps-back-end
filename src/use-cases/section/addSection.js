e0153f15ae3c5a76e187b30be8b54025951301e8
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-14 16:55:54 -0500

    Add two-way binding for course to section

--- src/use-cases/section/addSection.js
+++ src/use-cases/section/addSection.js
@@ -1,11 +1,25 @@
-module.exports = makeAddSection = ({ Section }) => {
+module.exports = makeAddSection = ({ Section, Course }) => {
 	// Save a new section in the db
 	// Resolve -> section object
 	// Reject -> error name
 	return async function addSection(sectionInfo = {}) {
 		return new Promise(async (resolve, reject) => {
+			const options = { returnDocument: 'after' }
+
 			const section = new Section(sectionInfo)
 			try {
+				// Add section to course.sections
+				const course = await Course.findByIdAndUpdate(
+					sectionInfo.courseId,
+					{
+						$push: { sections: section._id },
+					},
+					options
+				)
+				if (course == null) {
+					throw ReferenceError('Course not found.')
+				}
+
 				const saved = await section.save()
 				return resolve(saved.toObject())
 			} catch (err) {
--- src/use-cases/section/deleteSection.js
+++ src/use-cases/section/deleteSection.js
@@ -1,14 +1,25 @@
-module.exports = makeDeleteSection = ({ Section }) => {
+module.exports = makeDeleteSection = ({ Section, Course }) => {
 	// Delete a section in the db
 	// Resolve -> {deleted: #}
 	// Rejects -> err.name
 	return async function deleteSection(id) {
 		return new Promise(async (resolve, reject) => {
+			const options = { returnDocument: 'after' }
+
 			try {
+				// Remove section from db
 				const section = await Section.findByIdAndDelete(id).exec()
 				if (section == null) {
 					return reject({ deleted: 0 })
 				}
+
+				// Remove section from course.sections
+				const course = await Course.findByIdAndUpdate(
+					section.courseId,
+					{ $pull: { sections: section._id } },
+					options
+				)
+
 				return resolve({ deleted: 1 })
 			} catch (err) {
 				return reject(err)
