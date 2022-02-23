d0ea57eb3daf3e9b92b1f4e264f6fce1b38d50dd
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-22 23:13:25 -0500

    Linting

--- src/use-cases/quizSubmission/addAnswers.test.js
+++ src/use-cases/quizSubmission/addAnswers.test.js
@@ -9,23 +9,23 @@ const makeAddAnswers = require('./addAnswers')
 
 describe('createSubmission Test Suite', () => {
 	const createSubmission = makeCreateSubmission({ QuizSubmission })
-    const addAnswers = makeAddAnswers({ QuizSubmission })
+	const addAnswers = makeAddAnswers({ QuizSubmission })
 
-    const { userId, quizId, quizVersionId } = makeFakeQuizSubmission()
-    const answers = [
-        {
-            questionNumber: 1,
-            answers: ['A','B']
-        },
-        {
-            questionNumber: 2,
-            answers: ['A']
-        },
-        {
-            questionNumber: 3,
-            answers: ['true']
-        }
-    ]
+	const { userId, quizId, quizVersionId } = makeFakeQuizSubmission()
+	const answers = [
+		{
+			questionNumber: 1,
+			answers: ['A', 'B'],
+		},
+		{
+			questionNumber: 2,
+			answers: ['A'],
+		},
+		{
+			questionNumber: 3,
+			answers: ['true'],
+		},
+	]
 
 	beforeAll(async () => {
 		await dbConnect()
@@ -41,36 +41,36 @@ describe('createSubmission Test Suite', () => {
 
 	it('successfully adds questions to a submission that has none to start', async () => {
 		const sub = await createSubmission(userId, quizId, quizVersionId)
-        const submission = await addAnswers(sub._id, answers)
+		const submission = await addAnswers(sub._id, answers)
 		expect(submission.userAnswers).not.toBeNull()
 		expect(submission.userAnswers[0].questionNumber).toBe(1)
 		expect(submission.userAnswers[1].questionNumber).toBe(2)
 		expect(submission.userAnswers[2].questionNumber).toBe(3)
 	})
-    
+
 	it('successfully adds questions to a submission in small increments', async () => {
 		const sub = await createSubmission(userId, quizId, quizVersionId)
-        await addAnswers(sub._id, [answers[0]])
-        const submission = await addAnswers(sub._id, [answers[1], answers[2]])
+		await addAnswers(sub._id, [answers[0]])
+		const submission = await addAnswers(sub._id, [answers[1], answers[2]])
 		expect(submission.userAnswers).not.toBeNull()
 		expect(submission.userAnswers[0].questionNumber).toBe(1)
 		expect(submission.userAnswers[1].questionNumber).toBe(2)
 		expect(submission.userAnswers[2].questionNumber).toBe(3)
 	})
-        
+
 	it('successfully adds questions to a submission by overriding when needed', async () => {
 		const sub = await createSubmission(userId, quizId, quizVersionId)
-        const newAnswer = {
-            questionNumber: 1,
-            answers: ['false']
-        }
-        await addAnswers(sub._id, [answers[0]])
-        const submission = await addAnswers(sub._id, [answers[1], newAnswer])
+		const newAnswer = {
+			questionNumber: 1,
+			answers: ['false'],
+		}
+		await addAnswers(sub._id, [answers[0]])
+		const submission = await addAnswers(sub._id, [answers[1], newAnswer])
 		expect(submission.userAnswers).not.toBeNull()
 		expect(submission.userAnswers.length).toBe(2)
 	})
 
-    it('fails to add questions when cannot find submission', async () => {
+	it('fails to add questions when cannot find submission', async () => {
 		let errorName = 'nothing'
 		try {
 			await addAnswers('6212c736e5877e3e5f1b3981', answers)
@@ -80,7 +80,7 @@ describe('createSubmission Test Suite', () => {
 		expect(errorName).toBe('ReferenceError')
 	})
 
-    it('fails to add questions when there are no questions to add', async () => {
+	it('fails to add questions when there are no questions to add', async () => {
 		let errorName = 'nothing'
 		try {
 			await addAnswers('6212c736e5877e3e5f1b3981')
@@ -89,8 +89,8 @@ describe('createSubmission Test Suite', () => {
 		}
 		expect(errorName).toBe('ReferenceError')
 	})
-    
-    it('fails to add questions there is no submissionId', async () => {
+
+	it('fails to add questions there is no submissionId', async () => {
 		let errorName = 'nothing'
 		try {
 			await addAnswers()
--- src/use-cases/quizSubmission/createSubmission.test.js
+++ src/use-cases/quizSubmission/createSubmission.test.js
@@ -22,34 +22,34 @@ describe('createSubmission Test Suite', () => {
 	})
 
 	it('successfully adds a quiz submission', async () => {
-        const { userId, quizId, quizVersionId } = makeFakeQuizSubmission()
+		const { userId, quizId, quizVersionId } = makeFakeQuizSubmission()
 		const submission = await createSubmission(userId, quizId, quizVersionId)
 		expect(submission.submissionNumber).toBe(1)
 		expect(submission).not.toBeNull()
 	})
 
-    it('successfully auto incrememnts a new submission on creation for a submission with the same quizId', async () => {
-        const { userId, quizId, quizVersionId } = makeFakeQuizSubmission()
+	it('successfully auto incrememnts a new submission on creation for a submission with the same quizId', async () => {
+		const { userId, quizId, quizVersionId } = makeFakeQuizSubmission()
 		const submission1 = await createSubmission(userId, quizId, quizVersionId)
 		const submission2 = await createSubmission(userId, quizId, quizVersionId)
 		expect(submission2.submissionNumber).toBe(2)
 		expect(submission2).not.toBeNull()
 	})
 
-    it('successfully auto incrememnts when 2 different users take the same quiz', async () => {
-        const { quizId, quizVersionId } = makeFakeQuizSubmission()
-        const userId1 = '6212c736e5877e3e5f1b3981'
-        const userId2 = '6212c736e5877e3e5f1b3983'
+	it('successfully auto incrememnts when 2 different users take the same quiz', async () => {
+		const { quizId, quizVersionId } = makeFakeQuizSubmission()
+		const userId1 = '6212c736e5877e3e5f1b3981'
+		const userId2 = '6212c736e5877e3e5f1b3983'
 		const submission1 = await createSubmission(userId1, quizId, quizVersionId)
 		const submission2 = await createSubmission(userId2, quizId, quizVersionId)
 		expect(submission1.submissionNumber).toBe(1)
 		expect(submission2.submissionNumber).toBe(1)
 	})
 
-    it('successfully auto incrememnts when the same user takes 2 different quizzes', async () => {
-        const { userId, quizVersionId } = makeFakeQuizSubmission()
-        const quizId1 = '6212c736e5877e3e5f1b3981'
-        const quizId2 = '6212c736e5877e3e5f1b3983'
+	it('successfully auto incrememnts when the same user takes 2 different quizzes', async () => {
+		const { userId, quizVersionId } = makeFakeQuizSubmission()
+		const quizId1 = '6212c736e5877e3e5f1b3981'
+		const quizId2 = '6212c736e5877e3e5f1b3983'
 		const submission1 = await createSubmission(userId, quizId1, quizVersionId)
 		const submission2 = await createSubmission(userId, quizId2, quizVersionId)
 		expect(submission1.submissionNumber).toBe(1)
@@ -66,7 +66,7 @@ describe('createSubmission Test Suite', () => {
 		expect(errorName).toBe('ReferenceError')
 	})
 
-    it('fails to add a quiz submission quizId is not provided', async () => {
+	it('fails to add a quiz submission quizId is not provided', async () => {
 		let errorName = 'nothing'
 		try {
 			await createSubmission('6212c736e5877e3e5f1b3981')
@@ -76,10 +76,13 @@ describe('createSubmission Test Suite', () => {
 		expect(errorName).toBe('ReferenceError')
 	})
 
-    it('fails to add a quiz submission quizVersionId is not provided', async () => {
+	it('fails to add a quiz submission quizVersionId is not provided', async () => {
 		let errorName = 'nothing'
 		try {
-			await createSubmission('6212c736e5877e3e5f1b3981', '6212c736e5877e3e5f1b3983')
+			await createSubmission(
+				'6212c736e5877e3e5f1b3981',
+				'6212c736e5877e3e5f1b3983'
+			)
 		} catch (error) {
 			errorName = error.name
 		}
