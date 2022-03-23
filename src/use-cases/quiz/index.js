f6e0fdfe205ecb083b9ebe0b169d37b48b0ad633
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-22 23:47:30 -0400

    Add findContentQuizBelongsTo

--- src/use-cases/quiz/index.js
+++ src/use-cases/quiz/index.js
@@ -1,13 +1,14 @@
 const Quiz = require('../../database/models/quiz.model')
 const QuizSubmission = require('../../database/models/quizSubmission.model')
 const QuizVersion = require('../../database/models/quizVersion.model')
-const QuizJson = require('../../utils/quiz/quizJson')
+const Content = require('../../database/models/content.model')
 
 const makeAddQuestions = require('./addQuestions')
 const makeAddQuiz = require('./addQuiz')
 const makeBatchUpdateQuestions = require('./batchUpdateQuestions')
 const makeCopyQuiz = require('./copyQuiz')
 const makeDeleteQuestions = require('./deleteQuestions')
+const makeFindContentQuizBelongsTo = require('./findContentQuizBelongsTo')
 const makeFindQuizById = require('./findQuizById')
 const makeGetAllSubmissionGrades = require('./getAllSubmissionGrades')
 const makeStartQuiz = require('./startQuiz')
@@ -17,6 +18,7 @@ const addQuiz = makeAddQuiz({ Quiz, QuizVersion })
 const batchUpdateQuestions = makeBatchUpdateQuestions({ Quiz, QuizVersion })
 const copyQuiz = makeCopyQuiz({ Quiz, QuizVersion })
 const deleteQuestions = makeDeleteQuestions({ Quiz, QuizVersion })
+const findContentQuizBelongsTo = makeFindContentQuizBelongsTo({ Quiz, Content })
 const findQuizById = makeFindQuizById({ Quiz, QuizVersion })
 const getAllSubmissionGrades = makeGetAllSubmissionGrades({
 	Quiz,
@@ -30,6 +32,7 @@ module.exports = {
 	batchUpdateQuestions,
 	copyQuiz,
 	deleteQuestions,
+	findContentQuizBelongsTo,
 	findQuizById,
 	getAllSubmissionGrades,
 	startQuiz,
