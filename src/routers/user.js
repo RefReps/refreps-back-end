5b385901be8243b4cbd12d0246bf0d82f8d24f6f
Hunter Durbin <hmdurbin@bsu.edu>
2022-04-10 17:58:50 -0400

    Add find user route

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,14 +15,29 @@ const useCases = require('../use-cases')
 const { User } = useCases
 
 // routes
-router.get('/', isAuthenticated, authorizeAdmin, async (req, res) => {
-	try {
-		const { users } = await User.getAll()
-		res.status(200).json(users)
-	} catch (error) {
-		res.status(500).json({ error: error.message })
-	}
-})
+
+router
+	// @route   GET api/user/
+	.get('/', isAuthenticated, authorizeAdmin, async (req, res) => {
+		try {
+			const { users } = await User.getAll()
+			res.status(200).json({ users: users })
+		} catch (error) {
+			res.status(500).json({ error: error.message })
+		}
+	})
+
+router
+	// @route   GET api/user/:id
+	.get('/:userId', isAuthenticated, authorizeAdmin, async (req, res) => {
+		try {
+			const { userId } = req.params
+			const user = await User.findUserById(userId)
+			res.status(200).json({ user: user })
+		} catch (error) {
+			res.status(500).json({ error: error.message })
+		}
+	})
 
 // export router
 module.exports = router
