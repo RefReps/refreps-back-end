29976e64002a2c9f5851da03e0302b4476b8274f
Hunter Durbin <hmdurbin@bsu.edu>
2022-04-11 20:18:01 -0400

    Add copyAnnouncement

--- src/use-cases/annoucement/index.js
+++ src/use-cases/annoucement/index.js
@@ -1,15 +1,18 @@
 const Announcement = require('../../database/models/announcement.model')
 
 const makeAddAnnouncement = require('./addAnnouncement')
+const makeCopyAnnouncement = require('./copyAnnouncement')
 const makeFindAnnouncementById = require('./findAnnouncementById')
 const makeUpdateAnnouncement = require('./updateAnnouncement')
 
 const addAnnouncement = makeAddAnnouncement({ Announcement })
+const copyAnnouncement = makeCopyAnnouncement({ Announcement })
 const findAnnouncementById = makeFindAnnouncementById({ Announcement })
 const updateAnnouncement = makeUpdateAnnouncement({ Announcement })
 
 module.exports = {
 	addAnnouncement,
+	copyAnnouncement,
 	findAnnouncementById,
 	updateAnnouncement,
 }
